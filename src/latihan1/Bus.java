fb994428ae8ef8ce7c92d3cf0dcc054e50872928
MochRiswan <moch_riswan_29rpl@studen.smktelkom-mlg.sch.id>
2021-02-26 16:39:50 +0700

    Latihan1

---
+++ src/latihan1/Bus.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1;
+
+
+public class Bus {
+    public int penumpang;
+    public int maxPenumpang;
+    
+    public void cetak(){
+        System.out.println("Penumpang bus sekarang adalah "+penumpang);
+        System.out.println("Penumpang maksimum seharusnya adalah "+maxPenumpang);
+    }
+}
